e3e52336389eb278e2aa9b74c23475d21551e689
stefankorun <korun_stefan@hotmail.com>
2014-06-04 02:29:19 +0200

    Dodaena mapa, rabotit.

--- src/mk/korun/najdismestuvanje/PropertiesActivity.java
+++ src/mk/korun/najdismestuvanje/PropertiesActivity.java
@@ -2,13 +2,49 @@ package mk.korun.najdismestuvanje;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class PropertiesActivity extends FragmentActivity {
 
+	// Google Map
+    private GoogleMap googleMap;
+  
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_properties);
+		try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 	
 }
